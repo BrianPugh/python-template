5448997a1168d8d705bbfaacd464e5094547a245
Brian Pugh <bnp117@gmail.com>
2022-05-17 19:19:14 -0700

    disable smartquotes in sphinx

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -44,6 +44,8 @@ templates_path = ["_templates"]
 # This pattern also affects html_static_path and html_extra_path.
 exclude_patterns = []
 
+smartquotes = False
+
 # Napoleon settings
 napoleon_google_docstring = True
 napoleon_numpy_docstring = True
