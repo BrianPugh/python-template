dc537862acf4afbe4e4ad795828a0896756c1c6e
Brian Pugh <bnp117@gmail.com>
2022-07-31 15:50:58 -0700

    add sphinx.ext.autodoc (#6)

--- docs/source/index.rst
+++ docs/source/index.rst
@@ -10,6 +10,7 @@ Welcome to pythontemplate's documentation!
 
    Installation
    Overview
+   api
 
 
 
--- pythontemplate/__init__.py
+++ pythontemplate/__init__.py
@@ -1,2 +1,4 @@
 # Don't manually change, let poetry-dynamic-versioning-plugin handle it.
 __version__ = "0.0.0"
+
+__all__ = []
