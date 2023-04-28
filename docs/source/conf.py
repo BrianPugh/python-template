7928efc86dec76e75b94cecb4aac7bc1a21c52d9
Brian Pugh <bnp117@gmail.com>
2023-04-28 09:07:49 -0700

    fix double __init__ doc

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -76,7 +76,6 @@ napoleon_attr_annotations = True
 autodoc_default_options = {
     "members": True,
     "member-order": "bysource",
-    "special-members": "__init__",
     "undoc-members": True,
     "exclude-members": "__weakref__",
 }
